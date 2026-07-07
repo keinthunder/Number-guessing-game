1db3416c71b33599415b4d511baa356add59564a
keinthunder <shankyshanverma@gmail.com>
2026-07-07 22:17:42 +0530

    Update number_guessing _game.py

--- number_guessing _game.py
+++ number_guessing _game.py
@@ -2,14 +2,14 @@ print("🎮 Welcome to the Number Guessing Game!")
 print("Guess a number between 1 and 100.\n")
 import random
 secret_number=random.randint(1,100)
-Tries=0
+tries=0
 while True:
-    Tries+=1
-    guess=int(input('enter your number between 1-100'))
+    tries+=1
+    guess=int(input('enter your number (1-100) '))
     if guess==secret_number:
-        print("Congratulations! 🎉 You guessed the correct number in", Tries, "tries.")
+        print("Congratulations! 🎉 You guessed the correct number in", tries, "tries.")
         break
     elif guess>secret_number:
-        print("Too high! 📉 Try a smaller number")
+        print("Too high! 📉 Try a smaller number ")
     else:
-        print("Too low! 📈 Try a larger number")
+        print("Too low! 📈 Try a larger number ")
